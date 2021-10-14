1b2a769712abe0b75b421482a506937b8786f44b
GreenCityLife <greencitylife.sketchub@gmail.com>
2021-10-14 19:41:09 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,7 @@
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix=";", activity=discord.Game(name="Geometry Dash"), status=discord.Status.idle)
-TOKEN = os.getenv("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix=";")
 
 @bot.event
 async def on_ready():
