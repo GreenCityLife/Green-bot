8f34147e547639028db588b79afe064809b8f9ab
GreenCityLife <greencitylife.sketchub@gmail.com>
2021-10-15 13:13:57 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,8 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready():
+    activity = discord.Game(name="Netflix") 
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
 @bot.command()
