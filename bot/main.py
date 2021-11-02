784618769ff86567cb80b12d9ea1d295134998e9
GreenCityLife <greencitylife.sketchub@gmail.com>
2021-11-02 13:20:57 +0530

    Failed

--- bot/main.py
+++ bot/main.py
@@ -14,13 +14,5 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("Pong!")
 
-@client.event 
-async def on_message(message): 
- if message.content.startswith(';help'):
-  embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
-  embedVar.add_field(name="Field1", value="hi", inline=False) 
-  embedVar.add_field(name="Field2", value="hi2", inline=False) 
-  await message.channel.send(embed=embedVar)
-
 if __name__ == "__main__":
     bot.run(TOKEN)
